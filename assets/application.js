7a787f52c0732bd0806f373c76acf46f906e2cf4
kelvinmomanyi <kelvinmata42@gmail.com>
2025-12-04 06:25:41 +0300

    new

--- assets/application.js
+++ assets/application.js
@@ -11,19 +11,19 @@ function openModal() {
 //   document.getElementById('myModal2').style.display = 'flex';
 // }
 
-function toggleForm(formToShow){
-const login=document.getElementById('login')
-const register=document.getElementById('register')
+function toggleForm(formToShow) {
+  const login = document.getElementById('login')
+  const register = document.getElementById('register')
 
 
 
-if(formToShow === 'register'){
-  login.classList.add('hidden')
-  register.classList.remove('hidden')
-}else {
-  login.classList.remove('hidden')
-  register.classList.add('hidden')
-}
+  if (formToShow === 'register') {
+    login.classList.add('hidden')
+    register.classList.remove('hidden')
+  } else {
+    login.classList.remove('hidden')
+    register.classList.add('hidden')
+  }
 }
 
 
@@ -39,9 +39,9 @@ function closeCartbar() {
 }
 
 // Close the modal if the overlay (background) is clicked
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target === document.getElementById('myModal')) {
-      closeModal();
+    closeModal();
   }
 };
 
@@ -58,7 +58,7 @@ const addEventOnElem = function (elem, type, callback) {
   if (!elem) {
     return;
   }
-  
+
   if (elem.length > 1) {
     for (let i = 0; i < elem.length; i++) {
       if (elem[i] && typeof elem[i].addEventListener === 'function') {
@@ -80,10 +80,10 @@ const addEventOnElem = function (elem, type, callback) {
 
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const navbar = document.querySelector("[data-navbar]");
-const navbarLinks =document.querySelectorAll("data-nav-link")
+const navbarLinks = document.querySelectorAll("data-nav-link")
 const overlay = document.querySelector("[data-overlay]")
 
-const toggleNavbar = function(){
+const toggleNavbar = function () {
   navbar.classList.toggle('active');
   overlay.classList.toggle('active');
 }
@@ -91,9 +91,9 @@ const toggleNavbar = function(){
 addEventOnElem(navTogglers, "click", toggleNavbar)
 
 
-const closeNavbar = function(){
-   navbar.classList.remove('active')
-   overlay.classList.remove('active')
+const closeNavbar = function () {
+  navbar.classList.remove('active')
+  overlay.classList.remove('active')
 }
 
 addEventOnElem(navbarLinks, "click", closeNavbar)
@@ -110,7 +110,7 @@ const headerActive = function () {
   if (window.scrollY > 150) {
     header.classList.add("active");
     backTopBtn.classList.add("active");
-    
+
   } else {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
@@ -121,13 +121,13 @@ addEventOnElem(window, "scroll", headerActive)
 
 let lastScrolledPos = 0;
 
-const headerSticky = function() {
-    if(lastScrolledPos >= window.scrollY){
-        header.classList.remove("header-hide")
-    }else {
-        header.classList.add("header-hide")
-    }
-    lastScrolledPos = window.scrollY;
+const headerSticky = function () {
+  if (lastScrolledPos >= window.scrollY) {
+    header.classList.remove("header-hide")
+  } else {
+    header.classList.add("header-hide")
+  }
+  lastScrolledPos = window.scrollY;
 }
 
 addEventOnElem(window, "scroll", headerSticky)
@@ -138,9 +138,9 @@ addEventOnElem(window, "scroll", headerSticky)
 
 const sections = document.querySelectorAll("[data-section]")
 
-const scrollReveal = function(){
+const scrollReveal = function () {
   for (let i = 0; i < sections.length; i++) {
-    if(sections[i].getBoundingClientRect().top < window.innerHeight/2){
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 2) {
       sections[i].classList.add("active")
     }
   }
@@ -199,7 +199,7 @@ addEventOnElem(window, "scroll", scrollReveal)
 //     // Assuming you have elements in your side cart with specific IDs or classes
 //     let cartItemElements = document.querySelectorAll('.cart-item');
 //     let cartSubtotalElement = document.getElementById('cart-subtotal');
-    
+
 //     // Loop through the cart items to update or fetch additional data if needed
 //     for (let [index, item] of cartData.items.entries()) {
 //       let itemElement = cartItemElements[index];
@@ -210,7 +210,7 @@ addEventOnElem(window, "scroll", scrollReveal)
 //         itemElement.querySelector('.cart-item-variant').textContent = item.variant_title;
 //         itemElement.querySelector('.cart-item-quantity').textContent = `Quantity: ${item.quantity}`;
 //         itemElement.querySelector('.cart-item-price').textContent = `${(item.price / 100).toFixed(2)} ${cartData.currency}`;
-        
+
 //         // Async fetch additional data if necessary (e.g., fetching an image)
 //         let imageUrl = await fetchImageUrl(item);
 //         itemElement.querySelector('.cart-item-image').src = imageUrl;
@@ -234,7 +234,7 @@ addEventOnElem(window, "scroll", scrollReveal)
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var loader = document.getElementById('loader');
   var cartContent = document.getElementById('cart-content');
 
@@ -242,7 +242,7 @@ document.addEventListener("DOMContentLoaded", function() {
   loader.style.display = 'block';
 
   // Simulate data fetching (you can replace this with actual data fetching logic)
-  setTimeout(function() {
+  setTimeout(function () {
     // Hide the loader
     loader.style.display = 'none';
 
@@ -255,9 +255,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.addEventListener("DOMContentLoaded", function () {
   AOS.init({
-      duration: 800, // Animation duration
-      easing: "ease-in-out", // Easing function
-      once: true, // Whether animation should happen only once
+    duration: 800, // Animation duration
+    easing: "ease-in-out", // Easing function
+    once: true, // Whether animation should happen only once
   });
 });
 
