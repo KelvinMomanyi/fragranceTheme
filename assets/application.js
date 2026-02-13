fe0f8ed09a05bd72c5ceb3b8f5ca30ca7a0dbd1a
KelvinMomanyi <kelvinmata42@gmail.com>
2026-02-13 14:38:47 +0300

    new

--- assets/application.js
+++ assets/application.js
@@ -6,25 +6,7 @@
   'use strict';
 
   /* --------------------------------
-   * Utility: Add event to element(s)
-   * -------------------------------- */
-  var addEventOnElem = function (elem, type, callback) {
-    if (!elem) return;
-    if (elem.length > 1) {
-      for (var i = 0; i < elem.length; i++) {
-        if (elem[i] && typeof elem[i].addEventListener === 'function') {
-          elem[i].addEventListener(type, callback);
-        }
-      }
-    } else {
-      if (typeof elem.addEventListener === 'function') {
-        elem.addEventListener(type, callback);
-      }
-    }
-  };
-
-  /* --------------------------------
-   * Modal toggle
+   * Modal & Cart Toggles
    * -------------------------------- */
   window.openModal = function () {
     var modal = document.getElementById('myModal');
