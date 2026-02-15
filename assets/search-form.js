1bfef4bc128ca2b49bd2b5a02a77ad50ea5dc16a
kelvinmomanyi <kelvinmata42@gmail.com>
2026-02-15 06:05:38 +0300

    new

---
+++ assets/search-form.js
@@ -0,0 +1,41 @@
+class SearchForm extends HTMLElement {
+    constructor() {
+        super();
+        this.input = this.querySelector('input[type="search"]');
+        this.resetButton = this.querySelector('button[type="reset"]');
+
+        if (this.input) {
+            this.input.form.addEventListener('reset', this.onFormReset.bind(this));
+            this.input.addEventListener('input', this.onChange.bind(this));
+        }
+    }
+
+    toggleResetButton() {
+        const resetIsHidden = this.resetButton.classList.contains('hidden');
+        if (this.input.value.length > 0 && resetIsHidden) {
+            this.resetButton.classList.remove('hidden');
+        } else if (this.input.value.length === 0 && !resetIsHidden) {
+            this.resetButton.classList.add('hidden');
+        }
+    }
+
+    onChange() {
+        this.toggleResetButton();
+    }
+
+    shouldResetForm() {
+        return !document.querySelector('[aria-selected="true"] a');
+    }
+
+    onFormReset(event) {
+        // Prevent default so the form reset doesn't happen, which allows
+        // us to handle the reset manually
+        event.preventDefault();
+        // Simply clear the input
+        this.input.value = '';
+        this.input.focus();
+        this.toggleResetButton();
+    }
+}
+
+customElements.define('search-form', SearchForm);
